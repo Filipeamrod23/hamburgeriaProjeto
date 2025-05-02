47cc37ffd274c3a16836793d2047e65ee7a2d4ef
Filipeamrod23 <filipeamrod@gmail.com>
2025-05-01 21:12:59 -0400

    Update Htmls, css's and js's

--- cardapio/cardapio.js
+++ cardapio/cardapio.js
@@ -6,10 +6,35 @@ let carrinho = [];
 let modal = document.querySelector('#carrinho-modal');
 let fecharModal = document.querySelector('.fechar');
 let totalCarrinho = document.querySelector('#total-carrinho')
-let listaCarrinho = document.querySelector("#lista-carrinho")
+let listaCarrinho = document.querySelector(".lista-carrinho")
 
-document.querySelectorAll('.adicionar-carrinho').forEach(btn =>{
-    btn.addEventListener('click', function(){
+document.querySelectorAll('.adicionar-carrinho').forEach(btn => {
+    btn.addEventListener('click', function () {
         modal.style.display = 'block'
+        const nome = this.dataset.nome;
+        const preco = parseFloat(this.dataset.preco);
+        carrinho.push({ nome, preco })
+
+        atualizarCarrinho()
     })
 })
+
+// Variáveis, constantes, arrays/vetores, objetos (estrutura de dados organizada em propriedade e função
+
+fecharModal.addEventListener('click', function () {
+    modal.style.display = 'none'
+})
+
+function atualizarCarrinho() {
+    listaCarrinho.innerHTML = '';
+    let total = 0;
+
+    carrinho.forEach(item => {
+        const li = document.createElement("li")
+        li.style.listStyle = "none"
+        li.textContent = `${item.nome} - R$ ${item.preco.toFixed(2)}`
+        listaCarrinho.appendChild(li)
+        total = total + item.preco
+    })
+    totalCarrinho.textContent = total.toFixed(2)
+}
